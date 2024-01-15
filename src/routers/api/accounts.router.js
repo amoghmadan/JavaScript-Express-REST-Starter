d42a7dc751ce56cafc68c62fe046309f2a6586d2
Amogh Madan <amoghmadaan@gmail.com>
2024-01-15 17:10:46 +0530

    Fix HTTP methods.

--- src/routers/api/accounts.router.js
+++ src/routers/api/accounts.router.js
@@ -6,7 +6,7 @@ import {authenticate} from '../../middlewares';
 // eslint-disable-next-line new-cap
 const accountsRouter = Router();
 accountsRouter.route('/login').post(accountsController.login);
-accountsRouter.route('/detail').post(authenticate, accountsController.detail);
-accountsRouter.route('/logout').post(authenticate, accountsController.logout);
+accountsRouter.route('/detail').get(authenticate, accountsController.detail);
+accountsRouter.route('/logout').delete(authenticate, accountsController.logout);
 
 export default accountsRouter;
