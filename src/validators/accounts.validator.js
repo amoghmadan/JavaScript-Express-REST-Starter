9097e108216db45594e9d0c1dde255098d676eb4
Amogh Madan <amoghmadaan@gmail.com>
2024-01-15 13:38:49 +0530

    Typo: joi -> Joi

--- src/validators/accounts.validator.js
+++ src/validators/accounts.validator.js
@@ -1,28 +1,28 @@
-import joi from 'joi';
+import Joi from 'joi';
 
 export default {
-  createSuperUser: joi.object({
-    email: joi.string().email().required().label('Email'),
-    firstName: joi.string().min(1).required().label('First name'),
-    lastName: joi.string().min(1).required().label('Last name'),
-    password: joi.string().min(8).max(15).required().label('Password'),
-    passwordAgain: joi
+  createSuperUser: Joi.object({
+    email: Joi.string().email().required().label('Email'),
+    firstName: Joi.string().min(1).required().label('First name'),
+    lastName: Joi.string().min(1).required().label('Last name'),
+    password: Joi.string().min(8).max(15).required().label('Password'),
+    passwordAgain: Joi
         .any()
-        .equal(joi.ref('password'))
+        .equal(Joi.ref('password'))
         .required()
         .label('Password (again)')
         .messages({'any.only': 'Error: Your passwords didn\'t match.'}),
   }),
-  login: joi.object({
-    email: joi.string().email().required().label('Email'),
-    password: joi.string().min(8).max(15).required().label('Password'),
+  login: Joi.object({
+    email: Joi.string().email().required().label('Email'),
+    password: Joi.string().min(8).max(15).required().label('Password'),
   }),
-  changePassword: joi.object({
-    email: joi.string().email().required().label('Email'),
-    password: joi.string().min(8).max(15).required().label('Password'),
-    passwordAgain: joi
+  changePassword: Joi.object({
+    email: Joi.string().email().required().label('Email'),
+    password: Joi.string().min(8).max(15).required().label('Password'),
+    passwordAgain: Joi
         .any()
-        .equal(joi.ref('password'))
+        .equal(Joi.ref('password'))
         .required()
         .label('Password (again)')
         .messages({'any.only': 'Error: Your passwords didn\'t match.'}),
