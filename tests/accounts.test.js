c1ea260670435a2be34ac871abebd2dde1caeab1
Amogh Madan <amoghmadaan@gmail.com>
2024-01-16 21:25:54 +0530

    Add coverage to check tests quality.

--- tests/accounts.test.js
+++ tests/accounts.test.js
@@ -30,6 +30,24 @@ describe('Account API Tests', () => {
   });
 
   describe(`POST ${ROOT_URL}/login`, () => {
+    it('Incomplete Payload', async () => {
+      const response = await request
+          .post(`${ROOT_URL}/login`)
+          .send({email: EMAIL});
+      expect(response.status).toBe(400);
+    });
+  });
+
+  describe(`POST ${ROOT_URL}/login`, () => {
+    it('Invalid Credentials', async () => {
+      const response = await request
+          .post(`${ROOT_URL}/login`)
+          .send({email: EMAIL, password: 'foobarba'});
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe(`POST ${ROOT_URL}/login`, () => {
     it('Performs Account Login', async () => {
       const response = await request
           .post(`${ROOT_URL}/login`)
@@ -59,6 +77,27 @@ describe('Account API Tests', () => {
       const response = await request.get(`${ROOT_URL}/detail`);
       expect(response.status).toBe(401);
     });
+
+    it('Returns 401 if Bearer instead of Token', async () => {
+      const response = await request
+          .get(`${ROOT_URL}/detail`)
+          .set('Authorization', 'Bearer Token');
+      expect(response.status).toBe(401);
+    });
+
+    it('Returns 401 if more than two value', async () => {
+      const response = await request
+          .get(`${ROOT_URL}/detail`)
+          .set('Authorization', 'This Bearer Token');
+      expect(response.status).toBe(401);
+    });
+
+    it('Returns 403 if invalid token', async () => {
+      const response = await request
+          .get(`${ROOT_URL}/detail`)
+          .set('Authorization', 'Token 0X1X2X3X4X5X6X7X8X');
+      expect(response.status).toBe(403);
+    });
   });
 
   describe(`DELETE ${ROOT_URL}/logout`, () => {
@@ -71,11 +110,6 @@ describe('Account API Tests', () => {
           .delete(`${ROOT_URL}/logout`)
           .set('Authorization', `${KEYWORD} ${user.token.key}`);
       expect(response.status).toBe(204);
-    });
-
-    it('Returns 401 if not authorized', async () => {
-      const response = await request.delete(`${ROOT_URL}/logout`);
-      expect(response.status).toBe(401);
     });
   });
 });
